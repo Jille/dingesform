ea58704671ed5adc46402d8e5b2d166a3ca6fbb3
Jille Timmermans <jille@quis.cx>
2009-04-26 13:31:04 +0000

    checks in registerField en addField

--- form.php
+++ form.php
@@ -8,16 +8,17 @@
 
 		function createField($type, $name, $required, $label) {
 			$class = 'Dinges'. $type .'Field';
-		/*
 			if(!is_subclass_of($class, 'DingesFormField')) {
 				throw new DingesException($type .' is not an subclass of DingesFormField');
 			}
-		*/
 			$field = new $class($name, $required, $label);
 			$this->addField($field);
 		}
 
 		function addField($field) {
+			if(!is_object($field) || !$field instanceof DingesFormField) {
+				throw new DingesException('Invalid field given to addField()');
+			}
 			if(isset($this->fields[$field->name])) {
 				throw new DingesException('There is already a field with the name: '. $field->name);
 			}
