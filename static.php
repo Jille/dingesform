9c860c353e2611bf08d68aa7106bddb2f7ff0061
Jille Timmermans <jille@quis.cx>
2009-04-26 21:56:29 +0000

    bugfix in static (nu werkt ie ook nog na een submit)

--- static.php
+++ static.php
@@ -1,7 +1,7 @@
 <?php
 	class DingesStatic extends DingesField {
 		function render() {
-			return DingesForm::generateTag('span', array('id' => $this->id), $this->getEffectiveValue());
+			return DingesForm::generateTag('span', array('id' => $this->id), $this->getDefaultValue());
 		}
 	}
 ?>
