56277297832666b4e0df5081476947ec56cdfd67
Jille Timmermans <jille@quis.cx>
2009-04-30 14:17:04 +0000

    getLabelTag() overriden voor static

--- static.php
+++ static.php
@@ -3,5 +3,9 @@
 		function render() {
 			return DingesForm::generateTag('span', array('id' => $this->f->fieldIdPrefix . $this->id), $this->getDefaultValue());
 		}
+
+		function getLabelTag() {
+			return DingesForm::generateTag('span', array('id' => $this->f->fieldIdPrefix .'label_'. $this->id), $this->getDefaultValue());
+		}
 	}
 ?>
