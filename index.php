e41ab175f503e8e17785dca6f3c05d3d0a350aa4
Jille Timmermans <jille@quis.cx>
2009-05-24 18:02:45 +0000

    standaard geen errorIcon gebruiken

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 
 	$f = new DingesForm();
 	$f->setFieldIdPrefix('blaat_');
-	$f->setErrorIcon('exclamation.png');
+	//$f->setErrorIcon('exclamation.png');
 
 	$f->setErrorMessage('ERR_UNSAFE', 'Het wachtwoord mag niet gelijk zijn aan de gebruikersnaam');
 
