0603a1b69805794151f57b9499cc7ee9349e79db
Jille Timmermans <jille@quis.cx>
2009-06-02 19:40:10 +0000

    DingesTextArea onder DingesText gehangen

--- textarea.php
+++ textarea.php
@@ -1,39 +1,14 @@
 <?php
-	class DingesTextarea extends DingesInputField {
-		protected $maxLength;
-		protected $minLength;
+	class DingesTextArea extends DingesText {
 		protected $cols = 60;
 		protected $rows = 4;
 
 		protected $element = 'textarea';
 		
-		function validate($value) {
-			if(($error = parent::validate($value)) !== true) {
-				return $error;
-			}
-			if($this->minLength && strlen($value) < $this->minLength) {
-				return 'ERR_UNDER_MINLENGTH';
-			}
-			if($this->maxLength && strlen($value) > $this->maxLength) {
-				return 'ERR_OVER_MAXLENGTH';
-			}
-			return true;
-		}
-
 		function fillAttributes() {
 			parent::fillAttributes();
 			$this->setAttribute('cols', $this->cols);
 			$this->setAttribute('rows', $this->rows);
-		}
-
-		function fillRestrictions() {
-			parent::fillRestrictions();
-			if(isset($this->maxLength) && $this->maxLength > 0) {
-				$this->setRestriction('maxLength', $this->maxLength);
-			}
-			if(isset($this->minLength) && $this->minLength > 0) {
-				$this->setRestriction('minLength', $this->minLength);
-			}
 		}
 
 		function generateHTML() {
@@ -41,14 +16,6 @@
 				$content = '';
 			}
 			return DingesForm::generateTag($this->element, $this->attributes, htmlspecialchars($content, ENT_NOQUOTES)) . $this->getRestrictionComment();
-		}
-
-		function setMaxLength($length) {
-			$this->maxLength = intval($length);
-		}
-
-		function setMinLength($length) {
-			$this->minLength = intval($length);
 		}
 
 		function setCols($nr) {
