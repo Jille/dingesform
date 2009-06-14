3740d48046b0dd6128b310592c945f24fbe569b1
Jille Timmermans <jille@quis.cx>
2009-06-14 13:08:00 +0000

    no-op construct van de checkbox weg

--- checkbox.php
+++ checkbox.php
@@ -1,9 +1,5 @@
 <?php
 	class DingesCheckbox extends DingesInputField {
-		function __construct($name, $required, $label) {
-			parent::__construct($name, $required, $label);
-		}
-
 		function getValue() {
 			return !is_null(parent::getValue());
 		}
